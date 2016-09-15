58a50fe912fe5feb9cbfedee5bf0d2b7a1a8c555
Steffen Hippeli <hippeli@edu-sharing.com>
2016-09-15 11:10:01 +0200

    add dynamic token to lock function/view add ucfirst for matadata labels as repository handles it inconsistently

--- modules/video/mod_video.php
+++ modules/video/mod_video.php
@@ -144,12 +144,11 @@ extends ESRender_Module_AudioVideo_Abstract
     	//load resource asynchr. with display mode inline!
     	$template_data['ajax_url'] = $ROOT_URI . 'application/esmain/index.php?'.'app_id='
     			.$requestData['app_id'].'&session='.$requestData['session']
-    			 .'&token=' . $requestData['token']
     			.'&rep_id='.$requestData['rep_id'].'&obj_id='.$requestData['object_id'].'&resource_id='
     					.$requestData['resource_id'].'&course_id='.$requestData['course_id'].'&version='.$requestData['version']
     					.'&display=inline&displayoption=min&language='.$Locale->getLanguageTwoLetters().'&u='.urlencode($requestData['user_name_encr']).'&antiCache=' . mt_rand();
     					//could be achieved with jquery ajax option, but in this way we can influence, for example allow caching if resource is in conversion cue
-    	
+    	$template_data['token'] = $requestData['token']; 	
     	$valuesToShow = array(
     					'AlfrescoMimeType',
     					'{http://www.alfresco.org/model/content/1.0}modified',
@@ -199,7 +198,7 @@ extends ESRender_Module_AudioVideo_Abstract
         $positionInConversionQueue = $this->_ESOBJECT->getPositionInConversionQueue($this-> getVideoFormatByRequestingDevice());
         if(empty($progress) || is_array($progress))
             $progress = '0';
-        echo $template->render('/module/video/lock', array('callback' => $requestData['callback'], 'progress' => $progress, 'positionInConversionQueue' => $positionInConversionQueue));
+        echo $template->render('/module/video/lock', array('callback' => $requestData['callback'], 'token' => $requestData['token'], 'progress' => $progress, 'positionInConversionQueue' => $positionInConversionQueue));
         return true;
     }
 

c855476e75356093906bcd22d4daea666a2e1994
Steffen Hippeli <hippeli@edu-sharing.com>
2016-11-02 12:27:44 +0100

    enhance accessibility

--- theme/default/js/dynamic.js
+++ theme/default/js/dynamic.js
@@ -4,15 +4,18 @@ function toggle_edusharing_rendering_metadata() {
 	if(el.style.display != 'none') {
 		el.style.display = 'none';
 		toptoggle.innerHTML = '<?php echo $msg['showInformation']->localize($Locale, $Translate);?>';
+		toptoggle.title = '<?php echo $msg['showInformation']->localize($Locale, $Translate);?>';
 	} else {
 		el.style.display = '';
 		toptoggle.innerHTML = '<?php echo $msg['hideInformation']->localize($Locale, $Translate);?>';
+		toptoggle.title = '<?php echo $msg['hideInformation']->localize($Locale, $Translate);?>';
 	}
 }
 
 function close_edusharing_rendering_metadata() {
 	document.getElementById('edusharing_rendering_metadata').style.display = 'none';
 	document.getElementById('edusharing_rendering_metadata_top_toggle').innerHTML = '<?php echo $msg['showInformation']->localize($Locale, $Translate);?>';
+	document.getElementById('edusharing_rendering_metadata_top_toggle').title = '<?php echo $msg['showInformation']->localize($Locale, $Translate);?>';
 	return true;
 }
 

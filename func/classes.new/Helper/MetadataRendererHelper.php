694140ba39bdcf7c543e7aae5b8bb7f362a2e239
Steffen Hippeli <hippeli@edu-sharing.com>
2017-01-23 16:28:03 +0100

    change labels

--- func/classes.new/Helper/MetadataRendererHelper.php
+++ func/classes.new/Helper/MetadataRendererHelper.php
@@ -2,15 +2,28 @@
 
 class MetadataRendererHelper {
 	
-	public static function render($metadata, $type = '', $cssClasses = array()) {
-		
+	public static function render($metadata, $type = '', $cssClasses = '', $alternativeLabel = '') {
+
+        $label = $metadata['label'];
+	    if(!empty($alternativeLabel)) {
+	        $label = $alternativeLabel;
+        }
+
+        if(is_array($cssClasses))
+            $cssClasses = implode(' ', $cssClasses);
+	    else
+            $cssClasses = $cssClasses;
+
+
+
+
 		switch($type) {
 			
 			case 'date':
 				
 				if(!empty($metadata)) {
-					return '<label class="edusharing_rendering_metadata_body_label">' . ucfirst($metadata['label']) . '</label>
-							<span class="edusharing_rendering_metadata_body_value ' . implode(' ', $cssClasses) . '">' . date('d.m.Y, H:i', strtotime($metadata['value'])) . '</span>';
+					return '<label class="edusharing_rendering_metadata_body_label">' . ucfirst($label) . '</label>
+							<span class="edusharing_rendering_metadata_body_value ' . $cssClasses . '">' . date('d.m.Y, H:i', strtotime($metadata['value'])) . '</span>';
 				}
 				
 			break;
@@ -22,14 +35,14 @@ class MetadataRendererHelper {
 					$valArray = explode('[#]', $metadata['value']);
 					foreach($valArray as $value) {
 						if(!empty($value))
-							$valString .= '<span class="edusharing_rendering_metadata_body_value ' . implode(' ', $cssClasses) . '">' . htmlentities($value) . '</span>';
+							$valString .= '<span class="edusharing_rendering_metadata_body_value ' . $cssClasses . '">' . htmlentities($value) . '</span>';
 					}
 				} else {
 					if(!empty(trim($metadata['value'])))
-						$valString = '<span class="edusharing_rendering_metadata_body_value ' . implode(' ', $cssClasses) . '">' . htmlentities($metadata['value']) . '</span>';
+						$valString = '<span class="edusharing_rendering_metadata_body_value ' . $cssClasses . '">' . htmlentities($metadata['value']) . '</span>';
 				}
 				if(!empty($valString))
-					return '<label class="edusharing_rendering_metadata_body_label">' . ucfirst($metadata['label']) . '</label>' . $valString;
+					return '<label class="edusharing_rendering_metadata_body_label">' . ucfirst($label) . '</label>' . $valString;
 		}
 		
 		return '';

5b033017b64ce16f05faa637b100c66fb367e5d0
Steffen Hippeli <hippeli@edu-sharing.com>
2017-09-27 12:13:44 +0200

    html module

--- locale/esmain/ZH/lang.common.php
+++ locale/esmain/ZH/lang.common.php
@@ -48,4 +48,5 @@ $Translate
 	->addTranslation('zh', 'toDownload', 'Download')
 	->addTranslation('zh', 'cannotOpenObject', '此材料无法在浏览器中显示')
 	->addTranslation('zh', 'cannotOpenObjectText', '为了使用材料，请下载它')
-	->addTranslation('zh', 'goToOrigin', 'Go to origin');
+	->addTranslation('zh', 'goToOrigin', 'Go to origin')
+    ->addTranslation('zh', 'showDocument', 'Show document');

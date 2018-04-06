fafdc9b7bcb5d73437f867749ed8f38d359df134
root <root@debian.localdomain>
2018-04-06 15:38:46 +0200

    fix mimetype

--- modules/h5p/redirect.php
+++ modules/h5p/redirect.php
@@ -1,23 +1,16 @@
-
 <?php
 require_once __DIR__ . '/../../conf/system.conf.php';
-
 $pathArr = explode('modules/h5p', $_REQUEST['ID']);
 $path = $CC_RENDER_PATH . '/h5p' . $pathArr[1];
-
-
 $filesize = filesize($path);
 
 $path_parts = pathinfo($path);
 if($path_parts['extension'] === 'css') {
-    $mime_type = 'text/css';
-    header("Content-type: " . $mime_type);
+    header("Content-type: text/css");
+} else {
+    header('Content-type: ' . mime_content_type($path));
 }
-
-
-header("Content-length: " . $filesize);
+header('Content-length: ' . $filesize);
 header('Access-Control-Allow-Origin: *');
-
-@readfile($path);
-
+echo file_get_contents($path);
 exit(0);

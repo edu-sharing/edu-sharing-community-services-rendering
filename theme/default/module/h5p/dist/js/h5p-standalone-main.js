03e3a19eb1a009eab8d4619b8430226b10ecf50f
shippeli <hippeli@edu-sharing.com>
2018-04-09 10:13:15 +0200

    js lib fix for inline rendering

--- theme/default/module/h5p/dist/js/h5p-standalone-main.js
+++ theme/default/module/h5p/dist/js/h5p-standalone-main.js
@@ -23,17 +23,17 @@
  *
  ****/
 (function( global, factory ) {
-    if( typeof define === "function" && define.amd ) {
-        define( "Toposort", ["exports", "module"], factory );
-    } else if( typeof exports !== "undefined" && typeof module !== "undefined" ) {
-        factory( exports, module );
-    } else {
+   // if( typeof define === "function" && define.amd ) {
+    //    define( "Toposort", ["exports", "module"], factory );
+   // } else if( typeof exports !== "undefined" && typeof module !== "undefined" ) {
+    //    factory( exports, module );
+   // } else {
         var mod = {
             exports: {}
         };
         factory( mod.exports, mod );
         global.Toposort = mod.exports;
-    }
+   // }
 })( this, function( exports, module ) {
     "use strict";
 

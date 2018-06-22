2f07505bf1b2916f2de092ce56945befc690e9f9
shippeli <hippeli@edu-sharing.com>
2018-06-22 10:33:38 +0200

    Revert "no message"
    This reverts commit 004d7e8a4364c45ef2c990de5aebd99bcc4b63a0.

--- modules/doc/mod_doc.php
+++ modules/doc/mod_doc.php
@@ -74,11 +74,10 @@ extends ESRender_Module_ContentNode_Abstract {
             }
 
             if($this->getDoctype() === DOCTYPE_TEXT) {
-                $template_data['content'] = nl2br(htmlentities(file_get_contents($this->getCacheFileName())));
+                $template_data['content'] = nl2br(file_get_contents($this->getCacheFileName()));
             }
 
         }   
-
 
         if(Config::get('showMetadata'))
         	$template_data['metadata'] = $this -> _ESOBJECT -> metadatahandler -> render($this -> getTemplate(), '/metadata/dynamic');

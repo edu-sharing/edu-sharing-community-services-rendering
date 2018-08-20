38f5a57fa08c36de23580d671e7ec939eadd9234
shippeli <hippeli@edu-sharing.com>
2018-08-20 17:01:15 +0200

    feature extended tracking chg parameters

--- func/classes.new/ESRender/Plugin/ExtendedTracking.php
+++ func/classes.new/ESRender/Plugin/ExtendedTracking.php
@@ -25,15 +25,17 @@ class ESRender_Plugin_ExtendedTracking extends ESRender_Plugin_Abstract {
         if(ENABLE_TRACK_OBJECT && DISABLE_TRACK_ANONYMIZATION) {
             $learningmodule = '';
             foreach (Config::get('renderInfoLMSReturn')->properties->item as $property) {
-                if($property->key == '{virtualproperty}primaryparent_nodeid')
+                if($property->key == '{http://www.campuscontent.de/model/1.0}learnunit_id')
                     $learningmodule = $property->value;
+                if($property->key == '{http://www.campuscontent.de/model/1.0}original')
+                    $element  = $property->value;
             }
             $extendedTrackingParams = array();
             $extendedTrackingParams['ESTRACK_PERSISTENTID'] = $params['user_id'];
             $extendedTrackingParams['ESTRACK_AFFILIATION'] = strtoupper(Config::get('renderInfoLMSReturn') -> eduSchoolPrimaryAffiliation);
             $extendedTrackingParams['ESTRACK_INSTITUTION'] = substr($params['user_id'], strpos($params['user_id'], INSTITUTION_SEAPARTOR) + 1);
             $extendedTrackingParams['ESTRACK_LEARNINGMODULE'] = $learningmodule;
-            $extendedTrackingParams['ESTRACK_ELEMENT'] = $params['object_id'];
+            $extendedTrackingParams['ESTRACK_ELEMENT'] = $element;
             $extendedTrackingParams['ESTRACK_VIEWTYPE'] = ($params['view_type'] == 'download') ? 'DOWNLOAD' : 'SHOW';
             Config::set('extendedTracking', $extendedTrackingParams);
         }

fa4038098b68f024988fab2c87b71b19308ce674
shippeli <hippeli@edu-sharing.com>
2019-01-30 13:15:34 +0100

    style h5p iframe / fullscreen

--- theme/default/css/dynamic.php
+++ theme/default/css/dynamic.php
@@ -179,6 +179,10 @@ a.dataProtectionRegulationsButton {
 	line-height: initial;
 }
 
+div.h5p-container {
+    margin-bottom: 30px;
+}
+
 @media (max-width: 900px) {
 
 	.edusharing_rendering_content_wrapper h3 {

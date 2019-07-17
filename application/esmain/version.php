0d8d29dcd764a1dab8046170e3f2b21be88f4534
Martin Tippmann <tippmann@edu-sharing.com>
2019-07-17 08:18:30 +0000

    fix version.php for php7.3

--- application/esmain/version.php
+++ application/esmain/version.php
@@ -1,10 +1,9 @@
 <?php
+header('Content-Type: application/json');
 require_once ('../../conf.inc.php');
 require_once ('../../admin/model/Version.php');
 
 $version=new Version();
 $info["version"]=$version->getInstalledVersion();
 
-header('Content-Type','application/json');
 echo json_encode($info);
-?>

3c365d10db9243af8c655711a2b494d63eaf5df1
tsimon <simon@edu-sharing.net>
2019-07-25 13:28:33 +0200

    fix:improve logging on unknown mimetypes

--- func/classes.new/ESObject.php
+++ func/classes.new/ESObject.php
@@ -459,7 +459,7 @@ class ESObject {
             }
         } else {
             if (!$this -> module -> setModuleByMimetype($this -> mimetype)) {
-                Logger::getLogger('de.metaventis.esrender.index') -> info('Could not set module by mimetype using default ("doc") module.');
+                Logger::getLogger('de.metaventis.esrender.index') -> info('Could not set module by mimetype "'.$this->mimetype.'" using default ("doc") module.');
                 $this -> module -> setName('doc');
             }
         }

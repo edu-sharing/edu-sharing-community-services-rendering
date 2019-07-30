770d4bab4e1b178f01217f13d1ddcd2a9fae0ea0
chr-ernst <ernst@edu-sharing.net>
2019-07-30 10:58:34 +0200

    fix: syntax-error etherpad

--- install/_tmpl/sql/pgsql/VERSION.sql
+++ install/_tmpl/sql/pgsql/VERSION.sql
@@ -1,2 +1,2 @@
 INSERT INTO "VERSION" ("VERSION_ID", "VERSION_IDENT", "VERSION_NAME", "VERSION_DESC", "VERSION_VNUMBER", "VERSION_VBUILD", "VERSION_PATCH", "VERSION_TYPE", "VERSION_AUTHOR", "VERSION_URI", "OWNER", "WGROUP", "ACCESS", "COURSE_ID", "DS_ID", "VIEW", "LAYOUT", "RELSTATE", "RELLANG", "RELUSER", "RELDATE", "INSDAT", "UPDDAT", "UPDUSER", "STATE", "LASTCHANGE") VALUES
-(1, 1, 'esrender', 'edu-sharing ', '4.2.2', 1, NULL, 'install', 'metaVentis GmbH', 'edu-sharing.net', 0, NULL, NULL, NULL, 0, NULL, NULL, 'N', NULL, NULL, NULL, NULL, NULL, NULL, 'Y', NULL);
+(1, 1, 'esrender', 'edu-sharing ', '4.2.3', 1, NULL, 'install', 'metaVentis GmbH', 'edu-sharing.net', 0, NULL, NULL, NULL, 0, NULL, NULL, 'N', NULL, NULL, NULL, NULL, NULL, NULL, 'Y', NULL);

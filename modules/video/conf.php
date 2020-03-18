5a48e93abc2ccc193e25df5f5b335ad564420a40
chr-ernst <ernst@edu-sharing.net>
2020-03-18 14:24:33 +0100

    fix: add ffmpeg path;

--- modules/video/conf.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-define('FFMPEG_BINARY', 'C:\ffmpeg-20190522-8f6e651-win64-static\bin\ffmpeg.exe');
-define('EXEC_TIMEOUT', 3600);
-define('OPTION_THREADS', 1);
---
+++ modules/video/config.example.php
@@ -0,0 +1,6 @@
+<?php
+
+//define('FFMPEG_BINARY', 'C:\ffmpeg-20190522-8f6e651-win64-static\bin\ffmpeg.exe');
+define('FFMPEG_BINARY', '/var/www/html/rendering-service/vendor/lib/converter/ffmpeg');
+define('EXEC_TIMEOUT', 3600);
+define('OPTION_THREADS', 1);

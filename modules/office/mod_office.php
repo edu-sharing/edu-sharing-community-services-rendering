a399c6b2f64db1361db300a9de4bffd61e406321
Edmond Kacaj <edmondikacaj@gmail.com>
2021-08-11 14:59:11 +0200

    feat(Preview Microsoft files) implement preview for microsoft files like .docx|.xlsx|.pptx

--- modules/office/mod_office.php
+++ modules/office/mod_office.php
@@ -19,9 +19,12 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  */
-
+require_once (__DIR__ . '/config.php');
 include_once ('../../conf.inc.php');
-
+define('DOCTYPE_DOCX', 'DOCTYPE_DOCX');
+define('DOCTYPE_XLSX', 'DOCTYPE_XLSX');
+define('DOCTYPE_PPTX', 'DOCTYPE_PPTX');
+define('DOCTYPE_UNKNOWN', 'DOCTYPE_UNKNOWN');
 
 /**
  * handles
@@ -31,8 +34,211 @@ include_once ('../../conf.inc.php');
  * @package core
  * @subpackage classes.new
  */
-class mod_office
-	extends ESRender_Module_ContentNode_Abstract
+class mod_office extends ESRender_Module_ContentNode_Abstract
 {
+    private $doctype;
+
+    /**
+     * Extension: set doctype
+     */
+    public function __construct($Name, ESRender_Application_Interface $RenderApplication, ESObject $p_esobject, Logger $Logger, Phools_Template_Interface $Template)
+    {
+        parent::__construct($Name, $RenderApplication, $p_esobject, $Logger, $Template);
+        $this->setDoctype();
+    }
+
+    public function process($p_kind, $locked = null)
+    {
+        global $requestingDevice;
+        // It will be alway embeded mode, since we will will just embeded those files
+        $p_kind = ESRender_Application_Interface::DISPLAY_MODE_EMBED;
+        parent::process($p_kind);
+        return true;
+    }
+
+    // final protected function dynamic()
+    // {
+    //     if ($this->getDoctype() === DOCTYPE_DOCX)
+    //     {
+    //         echo $this->renderTemplate($this->getThemeByDoctype() . 'dynamic');
+    //         return true;
+    //     }
+    //     else if ($this->getDoctype() === DOCTYPE_XLSX)
+    //     {
+    //         echo $this->renderTemplate($this->getThemeByDoctype() . 'dynamic');
+    //         return true;
+    //     }
+    //     else if ($this->getDoctype() === DOCTYPE_PPTX)
+    //     {
+    //         echo $this->renderTemplate($this->getThemeByDoctype() . 'dynamic');
+    //         return true;
+    //     }
+    //     else return parent::dynamic();
+    // }
+    final protected function embed()
+    {
+        if ($this->getDoctype() === DOCTYPE_DOCX || $this->getDoctype() === DOCTYPE_XLSX || $this->getDoctype() === DOCTYPE_PPTX)
+        {
+            echo $this->renderTemplate($this->getThemeByDoctype() . 'embed');
+            return true;
+        }
+        else return parent::embed();
+    }
+
+    /**
+     * Set doctype
+     */
+    protected function setDoctype()
+    {
+        if (strpos($this
+            ->esObject
+            ->getMimeType() , 'application/msword') !== false || strpos($this
+            ->esObject
+            ->getMimeType() , 'application/vnd.openxmlformats-officedocument.wordprocessingml.document') !== false) $this->doctype = DOCTYPE_DOCX;
+        else if (strpos($this
+            ->esObject
+            ->getMimeType() , 'application/vnd.ms-excel') !== false || strpos($this
+            ->esObject
+            ->getMimeType() , 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet') !== false) $this->doctype = DOCTYPE_XLSX;
+        else if (strpos($this
+            ->esObject
+            ->getMimeType() , 'application/vnd.ms-powerpoint') !== false || strpos($this
+            ->esObject
+            ->getMimeType() , 'application/vnd.openxmlformats-officedocument.presentationml.presentation') !== false) $this->doctype = DOCTYPE_PPTX;
+        else $this->doctype = DOCTYPE_UNKNOWN;
+
+        return;
+    }
+
+    /** 
+     * Doctype getter
+     */
+    protected function getDoctype()
+    {
+        if (!$this->doctype) $this->setDoctype();
+        return $this->doctype;
+    }
+
+    /**
+     * Load theme according to current doctype
+     */
+    protected function getThemeByDoctype()
+    {
+        if (Config::get('hasContentLicense') === false) return '/module/default/';
+        switch ($this->getDoctype())
+        {
+            case DOCTYPE_DOCX:
+                return '/module/office/docx/';
+            break;
+            case DOCTYPE_PPTX:
+                return '/module/office/pptx/';
+            break;
+            case DOCTYPE_XLSX:
+                return '/module/office/xlsx/';
+            break;
+            default:
+                return '';
+        }
+    }
+
+    private function getFileType()
+    {
+        switch ($this->getDoctype())
+        {
+            case DOCTYPE_DOCX:
+                return 'docx';
+            break;
+            case DOCTYPE_PPTX:
+                return 'pptx';
+            break;
+            case DOCTYPE_XLSX:
+                return 'xlsx';
+            break;
+            default:
+                return 'docx';
+        }
+    }
+
+    /**
+     * Function for rendering Templates in base of MIME-TYPES
+     */
+    protected function renderTemplate($TemplateName, $showMetadata = true)
+    {
+        global $MC_URL;
+        $template_data = parent::prepareRenderData($showMetadata);
+        $template_data['previewUrl'] = $this
+            ->esObject
+            ->getPreviewUrl();
+        $template_data['logoImageUrl'] = $this->getConfiguration('logoImageUrl', $MC_URL . "/admin/img/edulogo.svg");
+        $template_data['logoLinkUrl'] = $this->getConfiguration('logoLinkUrl', "https://edu-sharing.com/");
+
+        if (Config::get('hasContentLicense') === true)
+        {
+
+            if ($this->getDoctype() == DOCTYPE_DOCX || $this->getDoctype() == DOCTYPE_PPTX || $this->getDoctype() == DOCTYPE_XLSX)
+            {
+
+                $urlFile = $this
+                    ->esObject
+                    ->getPath() . '?' . session_name() . '=' . session_id() . '&token=' . Config::get('token');
+                $template_data['url'] = $urlFile;
+                $template_data['title'] = $this
+                    ->esObject
+                    ->getTitle();
+                $template_data['objectId'] = $this
+                    ->esObject
+                    ->getObjectID();
+                $template_data['fileType'] = $this->getFileType();
+                $template_data['downloadAdvice'] = $this->downloadAdvice($template_data);
+
+            }
+
+        }
+
+        if (Config::get('showMetadata')) $template_data['metadata'] = $this
+            ->esObject
+            ->getMetadataHandler()
+            ->render($this->getTemplate() , '/metadata/dynamic');
+
+        $Template = $this->getTemplate();
+        $rendered = $Template->render($TemplateName, $template_data);
+        return $rendered;
+    }
+
+    /**
+     * This function will check for a specific configuration
+     * @param Type $configName configuration name
+     * @param Type $defaultValue a default value as fallback
+     * @return Type configuration values if exist
+     * @return Type default value if not exist
+     */
+    private function getConfiguration($configName, $defaultValue)
+    {
+        if (Config::get($configName) != "" || null != Config::get($configName)) return Config::get($configName);
+        else return $defaultValue;
+    }
+
+    /**
+     * function that return a html if something goes wrong with preview
+     * @param Array $dataArray keep a list of pair key:value
+     * @return a html string
+     */
+    private function downloadAdvice($dataArray)
+    {
+        global $Locale, $Translate;
+        $toDownload = new Phools_Message_Default('toDownload');
+        $cannotOpenObject = new Phools_Message_Default('cannotOpenObject');
+        $cannotOpenObjectText = new Phools_Message_Default('cannotOpenObjectText');
+        $downloadbutton = "";
+
+        if (Config::get('showDownloadButton') && Config::get('renderInfoLMSReturn')->hasContentLicense === true)
+        {
+            $downloadbutton = '<a href="' . $dataArray['url'] . '" download="' . $dataArray['title'] . '" class="edusharing_rendering_content" id="edusharing_rendering_content_href">' . $toDownload->localize($Locale, $Translate) . '</a>';
+        }
+
+        return '<div id="edusharing_downloadadvice">' . '<img class="edusharing_rendering_content_preview" src="' . $dataArray['previewUrl'] . '">' . '<h3>' . $cannotOpenObject->localize($Locale, $Translate) . '</h3>' . '<h4>' . $cannotOpenObjectText->localize($Locale, $Translate) . '</h4>' . '</div>' . $downloadbutton;
+
+    }
+
 }
 
---
+++ theme/default/module/office/js/initDocument.js
@@ -0,0 +1,123 @@
+/* initDocument.js
+ *   by Edmond Kacaj <edmondikacaj@gmail.com>
+ *
+ * Created:
+ *   7/5/2021, 12:31:07 PM
+ * Last edited:
+ *   7/5/2021, 12:31:08 PM
+ * Auto updated?
+ *   NO
+ *
+ * Description:
+ *   This file will contains all the configuration and script need for
+ *   loading DOCX,XLSX,PPTX documents type 
+ **/
+
+(function (global, $) {
+  // Those are default configurations 
+  // if we want to have more configuration we can do directly by setting them in HTML files, even if not exist here
+  // example: DocsServer.docsPermissions.help=true  activate help section
+  var DocsServer = {
+    // Configure Permsions
+    // see the onlyoffice  documentation https://api.onlyoffice.com/editors/config/document/permissions
+    docsPermissions: {
+      comment: false,
+      copy: false,
+      download: false,
+      edit: false,
+      review: false,
+      comments: false,
+      print: false
+    },
+    docsEditorConfig: {
+      lang: "en", //default
+      mode: "view", // always view
+      // Configure Customization Here
+      // see the onlyoffice  documentation https://api.onlyoffice.com/editors/config/editor/customization
+      customization: {
+        chat: false,
+        help: false,
+        hideRightMenu: true,
+        plugins: false,
+        toolbarHideFileName: false,
+        compactToolbar: true,
+        plugins: false,
+        reviewDisplay: "original",
+        trackChanges: false,
+        uiTheme: "theme-dark",
+        zoom: -2,
+        logo: {
+          image: "",
+          imageEmbedded: "",
+          url: ""
+        }
+      },
+      events: {
+        onError: function (e) {
+          console.log("DocsAPI==>onError Event");
+        },
+        onDocumentReady: function (e) {
+          console.log("DocsAPI==>onDocumentReady Event");
+        }
+      }
+    },
+    docsURL: "", // default empty string 
+    title: "",
+    docsType: "embedded", //default embeded 
+    docsExt: "docx",
+    docsPlaceHolder: "placeholder",
+    height: "100%",
+    width: "100%",
+    date: new Date()
+  };
+
+  DocsServer.DocsEditor = {
+    LoadDocuments: function () {
+      if (!DocsServer.docsURL || DocsServer.docsURL === "" || DocsServer.docsURL === undefined)
+        throw 'URL can\'t be empty!';
+
+      global.docEditor = new DocsAPI.DocEditor(DocsServer.docsPlaceHolder, {
+        type: DocsServer.docsType,
+        width: DocsServer.height,
+        height: DocsServer.width,
+        documentType: DocsServer.Helper.getDocumentType(DocsServer.docsExt),
+        document: {
+          title: DocsServer.docsTitle,
+          url: DocsServer.docsURL,
+          fileType: DocsServer.docsExt,
+          key: DocsServer.Helper.getDocumentKey(new Date().toString()),
+          permissions: DocsServer.docsPermissions
+        },
+        editorConfig: DocsServer.docsEditorConfig,
+        events: DocsServer.docsEditorConfig.events
+      });
+    }
+  };
+
+  DocsServer.Helper = {
+    /**
+     * Function that will return document type base of file type 
+     * @param {*} ext document extension exp: .xlsx || xls , doc || docx etc.
+     * @returns  return doc|| docx ==> word
+     */
+    getDocumentType: function (ext) {
+      if (".doc.docx.docm".indexOf(ext) != -1) return "word";
+      if (".xls.xlsx.xlt.csv".indexOf(ext) != -1) return "cell";
+      if (".pps.ppsx.ppt.pptx".indexOf(ext) != -1) return "slide";
+      return null;
+    },
+
+    /**
+     * --- Not requeired
+     * Function that will genarete a random key,
+     * @param {String} k , contains string that will converted to a uniq key
+     * @returns return auniq key 
+     */
+    getDocumentKey: function (k) {
+      var result = k.replace(new RegExp("[^0-9-.a-zA-Z_=]", "g"), "_") + (new Date()).getTime();
+      return result.substring(result.length - Math.min(result.length, 20));
+    }
+  };
+
+  window.DocsServer = DocsServer;
+})(window, $);

b6d6c70d57768c3e20a5d83e55f71c36e1601825
Torsten Simon <simon@edu-sharing.net>
2021-08-16 13:06:24 +0200

    use viewerjs for pdf

---
+++ vendor/js/ViewerJS/index.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * custom file to override styles provided by ViewerJS
+ */
+readfile('index.html');
+
+?>
+<style>
+    #titlebar, #toolbarRight {
+        display: none;
+    }
+</style>

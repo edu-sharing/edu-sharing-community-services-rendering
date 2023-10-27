430822a4e85bc8136a777eca9b9132564baa995e
ziegler <ziegler@edu-sharing.net>
2023-10-27 08:46:16 +0200

    Longer timeout for render moodle call

--- src/main/php/modules/moodle/mod_moodle.php
+++ src/main/php/modules/moodle/mod_moodle.php
@@ -135,7 +135,7 @@ extends ESRender_Module_ContentNode_Abstract {
 		curl_setopt($ch, CURLOPT_POSTFIELDS, $params);
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 		curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 90);
-		curl_setopt($ch, CURLOPT_TIMEOUT, 90 );
+		curl_setopt($ch, CURLOPT_TIMEOUT, 600 );
 		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 		$resp = curl_exec($ch);
 		echo curl_error($ch);

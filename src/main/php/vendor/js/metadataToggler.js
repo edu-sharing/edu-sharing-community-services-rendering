ca5fb4adebb0465f498948ba1210dfd411384874
ziegler <ziegler@edu-sharing.net>
2024-04-02 14:16:58 +0200

    feature: metadata toggle js as script

---
+++ src/main/php/vendor/js/metadataToggler.js
@@ -0,0 +1,11 @@
+document.addEventListener('click', event => {
+    // Case 1: toggle button was clicked -> show/hide the respective node's metadata div
+    if (event.target.classList.contains("edusharing_metadata_toggle_button")) {
+        const metadataNode = event.target.parentNode.querySelector(".edusharing_metadata")
+        metadataNode.style.display = metadataNode.style.display === 'none' ? '' : 'none';
+        // Case 2: Outside click -> Close all metadata divs
+    } else if (event.target.closest(".edusharing_metadata") === null) {
+        const allMetadataNodes = document.querySelectorAll(".edusharing_metadata")
+        allMetadataNodes.forEach(node => node.style.display = 'none')
+    }
+})

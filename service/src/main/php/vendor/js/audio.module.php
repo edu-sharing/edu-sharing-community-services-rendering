5f45384ad7536860234ba707c9461d3b630aa561
Torsten Simon <simon@edu-sharing.net>
2024-07-04 12:30:58 +0200

    fix:delegate audio inline script to fixed script src (for proper csp enforcing)

---
+++ service/src/main/php/vendor/js/audio.module.php
@@ -0,0 +1,16 @@
+<?php
+require_once (__DIR__ . '/../../conf.inc.php');
+session_id($_GET["PHPSESSID"]);
+session_start();
+$data = $_SESSION["mod_audio"][$_GET["ID"]];
+?>
+get_resource = function(authstring) {
+jQuery.ajax({
+url:"<?php echo $data["ajax_url"] ?>&callback=get_resource&"+authstring,
+success:function(data){
+jQuery("#edusharing_rendering_content").html(data);
+}
+});
+}
+get_resource("<?php echo $data["authString"] ?>");
+

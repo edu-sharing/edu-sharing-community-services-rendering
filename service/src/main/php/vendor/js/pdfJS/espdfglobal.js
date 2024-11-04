2718cd6e01f1e9deb2e2182b1e53fd42c28fb2b8
ziegler <ziegler@edu-sharing.net>
2024-11-04 11:22:11 +0100

    fix: inline style for pdf with no download/print rights removed

--- service/src/main/php/vendor/js/pdfJS/espdfglobal.js
+++
@@ -1,5 +0,0 @@
-let customEduStyle = document.createElement('style');
-customEduStyle.type = 'text/css';
-customEduStyle.innerText = '* { display: none  !important; }';
-customEduStyle.setAttribute('media', 'print');
-document.head.appendChild(customEduStyle);

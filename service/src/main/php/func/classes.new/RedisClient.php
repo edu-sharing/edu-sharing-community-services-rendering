54059db51297dea1956b755a153729754706fe14
Torsten Simon <simon@edu-sharing.net>
2026-01-30 14:54:12 +0100

    fix:redis client not cluster aware

--- service/src/main/php/func/classes.new/RedisClient.php
+++ service/src/main/php/func/classes.new/RedisClient.php
@@ -58,6 +58,7 @@ final class RedisClient
         }
         return new \Predis\Client([
             'scheme' => 'tcp',
+            'cluster' => 'redis',
             'host' => $host,
             'port' => $port,
             'persistent' => true,
